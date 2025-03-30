30a0b8bba134ffb2fed6ef09d93a62cd93fcc584
ZekturCh <jchavezch01@outlook.com>
2025-03-30 08:51:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //java
+let seccionActual = 0;
+const totalSecciones = document.querySelectorAll(".section").length;
+
+function cambiarSeccion(direccion) {
+    seccionActual += direccion;
+    
+    // Evitar salir de los límites
+    if (seccionActual < 0) seccionActual = 0;
+    if (seccionActual >= totalSecciones) seccionActual = totalSecciones - 1;
+
+    // Mueve la pantalla con GSAP
+    gsap.to(".container", { x: -seccionActual * window.innerWidth, duration: 1, ease: "power2.inOut" });
+}
