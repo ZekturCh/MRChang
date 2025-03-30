3edeee83f50ca4d81408be8eb35b525a3af17d9c
ZekturCh <jchavezch01@outlook.com>
2025-03-30 08:52:04 -0500

    Delete codes1.js

--- codes1.js
+++
@@ -1,7 +0,0 @@
-{
-  "codes": [
-    { "code": "ABC123", "url": "https://zekturch.github.io/ARBK-Web/Filtro1/" },
-    { "code": "XYZ789", "url": "/Filtro2" },
-    { "code": "DEF456", "url": "/Filtro3" }
-  ]
-}
